da34d8259de8c00f1e8138c9c98a1e864ab87c25
github-action-benchmark <github@users.noreply.github.com>
2024-06-17 05:06:22 +0000

    add Benchmark (benchmarkjs) benchmark result for 092d4501ccf0e43001f2cefb0860b1bcbb0f1485

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1718600782488,
+  "repoUrl": "https://github.com/ArchanaArige/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "092d4501ccf0e43001f2cefb0860b1bcbb0f1485",
+          "message": "ci(github): add workflow for DCO check\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-06-14T08:52:05-07:00",
+          "tree_id": "e59a83c4ef3ce206b28d85053c8bca7119a4c82d",
+          "url": "https://github.com/ArchanaArige/cacti/commit/092d4501ccf0e43001f2cefb0860b1bcbb0f1485"
+        },
+        "date": 1718600779909,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 560,
+            "range": "±1.71%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 345,
+            "range": "±1.39%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
