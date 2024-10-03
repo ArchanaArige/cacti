d4b8523e211276793a4bed91fa1fda768d21e9bc
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 11:48:10 +0000

    add Benchmark (benchmarkjs) benchmark result for 164addfdf625a0ed0d187a911b42a95875d7afa0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727955256518,
+  "lastUpdate": 1727956090334,
   "repoUrl": "https://github.com/ArchanaArige/cacti",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±2.93%",
             "unit": "ops/sec",
             "extra": "182 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "164addfdf625a0ed0d187a911b42a95875d7afa0",
+          "message": "test(cmd-api-server): jestify install-basic-plugin-consortium-manual\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-10-02T12:32:02-07:00",
+          "tree_id": "5e6359f77782a5bc2e8e8c3ec6ffec5e45658428",
+          "url": "https://github.com/ArchanaArige/cacti/commit/164addfdf625a0ed0d187a911b42a95875d7afa0"
+        },
+        "date": 1727956087638,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 684,
+            "range": "±2.69%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
